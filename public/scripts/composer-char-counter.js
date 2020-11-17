44e53dc26440a11287f1ce33476cb08052bd5bfa
Sunny (Hui-Shan) Cheng <huishan.cheng@studio.unibo.it>
2020-11-17 22:34:06 +0000

    adding key enter count with changing features of the number and the colour

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  console.log("I'm ready.");
+  
+  $('#tweet-text').on('input', function() {
+  let keyEn = $(this).val().length;
+  //console.log(140 - keyEn);
+  //$('.counter').val(140 - keyEn);
+  
+  if (keyEn <= 140) {
+    $("section.new-tweet").find("output").val(140 - keyEn).css("color","#545149");
+  } else {
+    $("section.new-tweet").find("output").val(140 - keyEn).css("color","red");
+  }
+ 
+
+  
+});
+
+
+
+});
+
+
