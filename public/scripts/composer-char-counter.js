18b6cebd8bf36f00b029d592a4b87a51f94e2345
Sunny (Hui-Shan) Cheng <huishan.cheng@studio.unibo.it>
2020-11-18 00:52:09 +0000

    adding the display with new tweet(hard-coded) with hover shadow

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
   let keyEn = $(this).val().length;
   //console.log(140 - keyEn);
   //$('.counter').val(140 - keyEn);
-  
   if (keyEn <= 140) {
     $("section.new-tweet").find("output").val(140 - keyEn).css("color","#545149");
   } else {
